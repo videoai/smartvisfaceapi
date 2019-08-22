3b09bae4803cde5a89957df22fa76309807522ce
Kieron Messer <kieron.messer@digitalbarriers.com>
2019-08-22 10:56:54 +1000

    Added Initial-USer-Agent header.  Properly escape strings.

--- csharp/SmartVisFaceAPI/Program.cs
+++ csharp/SmartVisFaceAPI/Program.cs
@@ -29,6 +29,11 @@ namespace SmartVisFaceAPI
         private static string client_secret = "2ae6ac4addaac04f688e6c6c17f6bcad501ace832f6da64e2d027f6d6322c8f6";
         private static string email = "demo@digitalbarriers.com";
         private static string password = "Demo123123";
+        //private static string auth_host = "https://auth-eudemo.videoai.net";
+        //private static string client_id = "43468dddbbf113ed312ba65dbe3dbb329a5cfb3115c6a6b4343b54f5fce551fc";
+        //private static string client_secret = "1e4fd81af8d0551f51d20040bdcdd4336ebe612f82d62f7953ac364c882fdb05";
+        //private static string email = "kieron.messer@digitalbarriers.com";
+        //private static string password = "kieron123!";
         private static string oauth_body_hash = "2jmj7l5rSw0yVb%2FvlWAYkK%2FYBwk%3D";
         private static string oauth_token = "a_single_token";
         private static string oauth_signature_method = "HMAC-SHA1";
@@ -92,8 +97,8 @@ namespace SmartVisFaceAPI
         {
             string auth_endpoint = "/auth/api_login";
             string post_data =
-                "email=" + email +
-                "&password=" + password;
+                "email=" + Uri.EscapeUriString(email) +
+                "&password=" + Uri.EscapeUriString(password);
             byte[] data = Encoding.ASCII.GetBytes(post_data);
 
             var oauth_nonce = GetNonce();
@@ -101,7 +106,7 @@ namespace SmartVisFaceAPI
             var method = "POST";
 
             string parameters =
-                "email=" + WebUtility.UrlEncode(email) +
+                "email=" + Uri.EscapeDataString(email) +
                 "&oauth_body_hash=" + oauth_body_hash +
                 "&oauth_consumer_key=" + client_id +
                 "&oauth_nonce=" + oauth_nonce +
@@ -109,12 +114,12 @@ namespace SmartVisFaceAPI
                 "&oauth_timestamp=" + oauth_timestamp +
                 "&oauth_token=" + oauth_token +
                 "&oauth_version=" + oauth_version +
-                "&password=" + WebUtility.UrlEncode(password);
+                "&password=" + Uri.EscapeDataString(password);
 
-            string base_string = method + "&" + WebUtility.UrlEncode(auth_endpoint) + "&" + WebUtility.UrlEncode(parameters);
+            string base_string = method + "&" + Uri.EscapeDataString(auth_endpoint) + "&" + Uri.EscapeDataString(parameters);
 
             string signed = CreateToken(base_string, client_secret + "&");
-            signed = WebUtility.UrlEncode(signed);
+            signed = Uri.EscapeDataString(signed);
 
             string authorization_header =
                 "OAuth realm=\"\"" +
@@ -131,6 +136,7 @@ namespace SmartVisFaceAPI
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
             request.Headers.Add("Authorization", authorization_header);
+            request.Headers.Add("Initial-User-Agent", "client_id=" + client_id);
             request.ContentLength = data.Length;
             using (Stream stream = request.GetRequestStream())
             {
@@ -147,13 +153,14 @@ namespace SmartVisFaceAPI
 
         private static bool ListSubjects(MySession mySession)
         {
-            var device_data = "device_id=\"id1234\", device_name=\"mydevice\", lat=\"0\", lng=\"0\"";
+            var device_data = Uri.EscapeDataString("device_id=\"id1234\", device_name=\"mydevice\", lat=\"0\", lng=\"0\"");
             var oauth_nonce = GetNonce();
             var oauth_timestamp = GetTimeStamp();
             var subject_end_point = "/subject";
             var method = "GET";
 
             var parameters =
+                //"device_data=" + device_data +
                 "oauth_body_hash=" + oauth_body_hash +
                 "&oauth_consumer_key=" + client_id +
                 "&oauth_nonce=" + oauth_nonce +
@@ -162,9 +169,9 @@ namespace SmartVisFaceAPI
                 "&oauth_token=" + mySession.token +
                 "&oauth_version=" + oauth_version;
             
-            var base_string = method + "&" + WebUtility.UrlEncode(subject_end_point) + "&" + WebUtility.UrlEncode(parameters);
+            var base_string = method + "&" + Uri.EscapeDataString(subject_end_point) + "&" + Uri.EscapeDataString(parameters);
             var signed = CreateToken(base_string, client_secret + "&");
-            signed = WebUtility.UrlEncode(signed);
+            signed = Uri.EscapeDataString(signed);
             
             // Lets make a request to list all the subjects in the database
             var authorization_header =
@@ -183,10 +190,11 @@ namespace SmartVisFaceAPI
             //request.ContentType = "application/x-www-form-urlencoded";
             //subject_request.Headers.Add("Device", "device_data=" + device_data);
             subject_request.Headers.Add("Authorization", authorization_header);
-
+            subject_request.Headers.Add("Initial-User-Agent", "client_id=" + client_id);
             var json_object = DoRequest(subject_request);
             return true;
         }
+       
         
         static void Main(string[] args)
         {
